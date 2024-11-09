1fffa154690a5dffe752adbb77799ebb16f0c6ad
mhndkptr <muhamadhendika77@gmail.com>
2024-11-09 15:05:39 +0700

    chore: fetch data trending in home page

--- src/components/section/home/HomeHeroSection.jsx
+++ src/components/section/home/HomeHeroSection.jsx
@@ -1,29 +1,45 @@
 import { FaCalendarAlt } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import defaultErrorBackdrop from "../../../assets/default-error-backdrop.jpg";
 
-export default function HomeHeroSection() {
+export default function HomeHeroSection({ highlightMovie }) {
+  const navigate = useNavigate();
+
   return (
     <>
       <section className="h-screen">
         <div className="h-full">
-          <img className="h-full w-screen absolute top-0 object-cover object-center -z-50" src="https://image.tmdb.org/t/p/original/b9UCfDzwiWw7mIFsIQR9ZJUeh7q.jpg" alt="Ghosted Img" />
+          <img
+            className="h-full w-screen absolute top-0 object-cover object-center -z-50"
+            src={highlightMovie.backdrop}
+            alt={highlightMovie.title}
+            onError={(error) => {
+              error.target.src = defaultErrorBackdrop;
+            }}
+          />
           <div className="absolute top-0 left-0 w-full h-full bg-[#0f2f3a] opacity-60 -z-40"></div>
 
           <div className="max-w-screen-xl mx-auto px-4 -translate-y-1/2 relative top-1/2">
             <div className="max-w-xl flex flex-col items-start gap-3 justify-center">
-              <h1 className="text-white font-extrabold text-5xl">Ghosted</h1>
+              <h1 className="text-white font-extrabold text-5xl">{highlightMovie.title}</h1>
               <div>
                 <div className="flex flex-row gap-2 justify-start items-center">
                   <FaCalendarAlt className="text-gray-300" />
                   <h4 className="text-gray-200">
-                    <span className="text-gray-300">Year: </span>2020
+                    <span className="text-gray-300">Year: </span>
+                    {highlightMovie.releaseDate.split("-")[0]}
                   </h4>
                 </div>
               </div>
-              <p className="text-gray-300">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique id nostrum, explicabo porro impedit rem repudiandae aperiam reiciendis harum voluptatibus voluptatum consectetur iusto fuga molestiae, corrupti suscipit
-                maxime tempora beatae.
-              </p>
-              <button className="text-gray-800 px-5 py-2 bg-[#fac54e] hover:bg-[#d6a940] hover:text-white transition font-medium rounded-lg">See Detail</button>
+              <p className="text-gray-300">{highlightMovie.overview}</p>
+              <button
+                onClick={() => {
+                  navigate(`/movie/${highlightMovie.id}`);
+                }}
+                className="text-gray-800 px-5 py-2 bg-[#fac54e] hover:bg-[#d6a940] hover:text-white transition font-medium rounded-lg"
+              >
+                See Detail
+              </button>
             </div>
           </div>
         </div>
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,13 +1,52 @@
+import { useEffect, useState } from "react";
 import HomeHeroSection from "../components/section/home/HomeHeroSection";
 import HomeTrendingSection from "../components/section/home/HomeTrendingSection";
+import request from "../utils/request";
+
+const tempHighlightMovie = {
+  id: 868759,
+  title: "Ghosted",
+  genres: ["Action & Adventure", "Comedy", "Romance"],
+  overview:
+    "Salt-of-the-earth Cole falls head over heels for enigmatic Sadie — but then makes the shocking discovery that she’s a secret agent. Before they can decide on a second date, Cole and Sadie are swept away on an international adventure to save the world.",
+  releaseDate: "2023-04-18",
+  poster: "https://image.tmdb.org/t/p/original/liLN69YgoovHVgmlHJ876PKi5Yi.jpg",
+  backdrop: "https://image.tmdb.org/t/p/original/b9UCfDzwiWw7mIFsIQR9ZJUeh7q.jpg",
+};
 
 export default function HomePage() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [highlightMovie, setHighlightMovie] = useState(null);
+
+  useEffect(() => {
+    request
+      .get("movie/trending")
+      .then((response) => {
+        if (response.data?.statusCode === 200 || response.data?.statusCode === 201) {
+          if (response.data.data.trendingMovies.length > 0) {
+            setHighlightMovie(response.data.data.trendingMovies[0]);
+            setTrendingMovies(response.data.data.trendingMovies.slice(1, 4));
+          } else {
+            setHighlightMovie(tempHighlightMovie);
+          }
+        } else {
+          setHighlightMovie(tempHighlightMovie);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setHighlightMovie(tempHighlightMovie);
+      });
+  }, []);
+
   return (
     <>
-      <div className="w-full">
-        <HomeHeroSection />
-        <HomeTrendingSection />
-      </div>
+      {highlightMovie && (
+        <div className="w-full">
+          <HomeHeroSection highlightMovie={highlightMovie} />
+          <HomeTrendingSection trendingMovies={trendingMovies} />
+        </div>
+      )}
     </>
   );
 }
